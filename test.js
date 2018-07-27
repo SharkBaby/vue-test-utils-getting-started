401e03a2e1aa99c299bf11a2ab8de6a3b9816c74
Perry Xiang <1142320771@qq.com>
2018-07-27 11:29:47 +0800

    1. fork 别人的仓库，并提交更新

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 // Import the mount() method from the test utils
 // and the component you want to test
+// update test from Perry
 import { mount } from '@vue/test-utils'
 import Counter from './counter'
 
